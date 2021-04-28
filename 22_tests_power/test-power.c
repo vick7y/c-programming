e54515f9e4cdae61cec5c697fdc214128bdf236b
Coursera Learner <nobody@nowhere.nul>
2021-04-28 20:24:18 +0000

    Did assignment 22

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+void run_check(unsigned x, unsigned y, unsigned expected_ans,int test_case_number){
+  unsigned ans=power(x,y);
+  if (ans != expected_ans){
+    // printf("Failed test case %d \n",test_case_number);
+    // printf("Got answer %u \n",ans);
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(){
+  run_check(0,0,1,1);
+  run_check(1,1,1,2);
+  run_check(2,0,1,3);
+  run_check(2,2,4,4);
+  run_check(-2,2,4,5);
+  run_check(10,10,1410065408,6);
+
+  return EXIT_SUCCESS;
+}
